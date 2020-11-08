e4250f45356198c21376ed5f47b2a8a81c6bcdb4
Matheus Braun <matheeusbl@gmail.com>
2020-11-07 23:51:38 -0300

    feat: adding container component

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css';
+
+const Container: React.FC = ({ children }) => (
+  <div className="container">{children}</div>
+);
+
+export default Container;
