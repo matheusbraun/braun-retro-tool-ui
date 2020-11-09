ccc6f81ab4bcae984f5fdf9736c32c3e4736d628
Matheus Braun <matheeusbl@gmail.com>
2020-11-08 23:36:34 -0300

    feat: adding panel to session

--- src/pages/Session/index.tsx
+++ src/pages/Session/index.tsx
@@ -1,13 +1,22 @@
 import React, { memo } from 'react';
 
+import Panel from '../../components/Panel';
 import Timer from '../../components/Timer';
 
 import './styles.css';
 
 const Session: React.FC = () => {
   return (
-    <div className="session-header">
-      <Timer />
+    <div className="session">
+      <div className="session-header">
+        <Timer />
+      </div>
+      <div className="divider" />
+      <div className="session-panels">
+        <Panel type="good" />
+        <Panel type="wondering" />
+        <Panel type="bad" />
+      </div>
     </div>
   );
 };
